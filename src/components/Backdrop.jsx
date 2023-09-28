f3e379ed73ea5af84395cf6e3a67579f027d04bd
William Hleucka <william.hleucka@gmail.com>
2023-09-27 18:55:16 -0600

    fix: background image delay (temp fix)

--- src/components/Backdrop.jsx
+++ src/components/Backdrop.jsx
@@ -5,17 +5,18 @@ import { FastAverageColor } from 'fast-average-color';
 const Backdrop = ({ backdropRef }) => {
     const { state } = useContext(SopranoContext);
     let backdropImage = '/img/no-album.png';
-    if (state.mode === 'search' || state.mode === 'playlist') {
-        if (Object.keys(state.track).length > 0 && state.track.cover) {
-            backdropImage =
-                process.env.REACT_APP_SERVER_URL +
-                '/api/v1/cover/' +
-                state.track.md5 +
-                '/500/500';
-        }
-    } else if (state.mode === 'radio' || state.mode === 'podcast') {
-        backdropImage = state.track.cover;
-    }
+    // if (state.mode === 'search' || state.mode === 'playlist') {
+    //     if (Object.keys(state.track).length > 0 && state.track.cover) {
+    //         backdropImage =
+    //             process.env.REACT_APP_SERVER_URL +
+    //             '/api/v1/cover/' +
+    //             state.track.md5 +
+    //             '/500/500';
+    //     }
+    // } else if (state.mode === 'radio' || state.mode === 'podcast') {
+    //     backdropImage = state.track.cover;
+    // }
+    backdropImage = state.track.cover;
 
     useEffect(() => {
         if (state.track) {
