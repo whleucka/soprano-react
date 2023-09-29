a3625a18f258c9688126196ecafee6834638d1de
William Hleucka <william.hleucka@gmail.com>
2023-09-29 16:37:09 -0600

    fix(track-row): overflow x

--- src/components/TrackRow.jsx
+++ src/components/TrackRow.jsx
@@ -1,7 +1,7 @@
 const TrackRow = (props) => {
     const { image, title, playtime, id = null } = props;
     return (
-        <div tabIndex="-1" id={'playlist-row-' +id} className="track-row d-flex align-items-center w-100">
+        <div tabIndex="-1" id={'playlist-row-' +id} className="track-row d-flex align-items-center flex-grow-1 truncate">
             <div className="image">{image}</div>
             <div
                 onClick={props.handleTitleClick}
